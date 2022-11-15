5ef8d0faf5f9aec45e703b896162f6a70bbb5ea2
Jose Luis Sanchez <jsanchezesc@uoc.edu>
2022-11-15 16:12:59 +0100

    added data.json tailwindcss and react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react';
-import './App.css';
+import { FaDiscord } from 'react-icons/fa'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Study-group-searcher</h1>
+    <div className="grid place-items-center">
+    <p className="text-4xl font-bold underline text-blue-600 decoration-orange-200 mt-24">
+      study group searcher <br/>
+      <span className="grid place-items-center " style={{color:"rgb(114, 137, 218)"}}><FaDiscord/></span>
+    </p>
+    
     </div>
   );
 }
